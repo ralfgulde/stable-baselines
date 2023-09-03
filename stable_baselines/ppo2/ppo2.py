36a093d7ba0b119f2e816391b37d530fe14f0005
Ralf Gulde <ralf.gulde@sereact.ai>
2023-09-03 22:42:08 +0200

    triangluar fixed

--- stable_baselines/ppo2/ppo2.py
+++ stable_baselines/ppo2/ppo2.py
@@ -67,7 +67,7 @@ class PPO2(ActorCriticRLModel):
         self.noptepochs = noptepochs
         self.tensorboard_log = tensorboard_log
         self.full_tensorboard_log = full_tensorboard_log
-        self.lr_cycle = cycling
+        self.lr_cycler = cycling
 
         self.action_ph = None
         self.advs_ph = None
@@ -262,6 +262,29 @@ class PPO2(ActorCriticRLModel):
                 approximation of kl divergence, updated clipping range, training update operation
         :param cliprange_vf: (float) Clipping factor for the value function
         """
+
+        def cyclic_lr(step, num_cycle_steps=10000, base_lr=5e-4, max_lr=1e-2):
+            mod_step = step % num_cycle_steps
+            half = num_cycle_steps / 2
+            mod_step_half = mod_step % half
+            pct = mod_step_half / half
+
+            if mod_step < half:
+                diff = max_lr - base_lr
+                diff = pct * diff
+                return base_lr + diff
+            else:
+                diff = max_lr - base_lr
+                diff = pct * diff
+                return max_lr - diff
+
+        if self.lr_cycler:
+        
+            learning_rate = cyclic_lr(update, self.lr_cycler[0], self.lr_cycler[1], self.lr_cycler[2])
+            #print('##########################')
+            #print("cycling: ", learning_rate, ", update: (",update, "/", self.lr_cycler[0],"), cyc: ", self.lr_cycler)
+            #print('##########################')
+
         advs = returns - values
         advs = (advs - advs.mean()) / (advs.std() + 1e-8)
         td_map = {self.train_model.obs_ph: obs, self.action_ph: actions,
@@ -301,7 +324,7 @@ class PPO2(ActorCriticRLModel):
         return policy_loss, value_loss, policy_entropy, approxkl, clipfrac
 
     def learn(self, total_timesteps, callback=None, log_interval=1, tb_log_name="PPO2_LR_CYCLED",
-              reset_num_timesteps=True, lr_cycler=False):
+              reset_num_timesteps=True):
         # Transform to callable if needed
         self.learning_rate = get_schedule_fn(self.learning_rate)
         self.cliprange = get_schedule_fn(self.cliprange)
@@ -309,21 +332,6 @@ class PPO2(ActorCriticRLModel):
 
         new_tb_log = self._init_num_timesteps(reset_num_timesteps)
         callback = self._init_callback(callback)
-
-        def cyclic_lr(step, num_cycle_steps=10000, base_lr=5e-4, max_lr=1e-2):
-            mod_step = step % num_cycle_steps
-            half = num_cycle_steps / 2
-            mod_step_half = mod_step % half
-            pct = mod_step_half / half
-
-            if mod_step < half:
-                diff = max_lr - base_lr
-                diff = pct * diff
-                return base_lr + diff
-            else:
-                diff = max_lr - base_lr
-                diff = pct * diff
-                return max_lr - diff
 
         with SetVerbosity(self.verbose), TensorboardWriter(self.graph, self.tensorboard_log, tb_log_name, new_tb_log) \
                 as writer:
@@ -344,11 +352,8 @@ class PPO2(ActorCriticRLModel):
                 t_start = time.time()
 
                 # Compute current learning_rate
-                if lr_cycler:
-                    lr_now = cyclic_lr(update)
-                else:
-                    frac = 1.0 - (update - 1.0) / n_updates
-                    lr_now = self.learning_rate(frac)
+                frac = 1.0 - (update - 1.0) / total_timesteps
+                lr_now = self.learning_rate(frac)
 
                 cliprange_now = self.cliprange(frac)
                 cliprange_vf_now = cliprange_vf(frac)
